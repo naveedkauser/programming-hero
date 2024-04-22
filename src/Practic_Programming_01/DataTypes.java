a56389d63b21d203cd776f80b11a01271e21efdf
ashhadhere <ashad.kauser@gmail.com>
2024-04-23 00:50:50 +0500

    explaining git commands and local variables

---
+++ src/Practic_Programming_01/DataTypes.java
@@ -0,0 +1,15 @@
+package Practic_Programming_01;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        //int variable
+        //declare and initialize
+
+        int num =10;
+        System.out.println("num is " +num);
+    }
+
+
+
+}
---
+++ src/Practic_Programming_01/Test_01.java
@@ -0,0 +1,12 @@
+package Practic_Programming_01;
+
+public class Test_01 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        String b = "naveed";
+        System.out.println("number is " + a);
+        System.out.println("name is " + b);
+
+    }
+}
